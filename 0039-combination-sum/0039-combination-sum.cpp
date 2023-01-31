e480f32c86d33459aa3009c7bf634c02511ba154
Akshit Saxena <102389628+saxenaakshit@users.noreply.github.com>
2023-02-01 00:32:04 +0530

    Time: 11 ms (52.04%), Space: 10.8 MB (81.77%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,43 +1,30 @@
+#include <bits/stdc++.h>
 class Solution {
+private:
+    void findsum(int ind,int target,vector<int>& candidates,vector<vector<int>> &ans,vector<int> &ds){
+        if(candidates.size()==ind){
+            if(target==0){
+                ans.push_back(ds);
+              
+            }
+              return;
+        }
+        
+        //picking up element
+        if(candidates[ind]<=target){
+            ds.push_back(candidates[ind]);
+            findsum(ind,target-candidates[ind],candidates,ans,ds);
+            ds.pop_back();
+        }
+        
+        //not picking
+        findsum(ind+1,target,candidates,ans,ds);
+    }
 public:
-
-    void Sum(vector<int>& candidates, int target, vector<vector<int> >& res, vector<int>& r, int i)
-    {
-        
-        if(target == 0)
-        {
-            // if we get exact answer
-            res.push_back(r);
-            return;
-        }
-        
-        while(i <  candidates.size() && target - candidates[i] >= 0)
-        {
-            // Till every element in the array starting
-            // from i which can contribute to the target
-            r.push_back(candidates[i]);// add them to vector
-            
-            // recur for next numbers
-            Sum(candidates,target - candidates[i],res,r,i);
-            ++i;
-            
-            // Remove number from vector (backtracking)
-            r.pop_back();
-        }
-}
-    
-     
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end()); // sort candidates array
-        
-        // remove duplicates
-        candidates.erase(unique(candidates.begin(),candidates.end()),candidates.end());
-        
-        vector<int> r;
-        vector<vector<int> > res;
-        
-        Sum(candidates,target,res,r,0);
-        
-        return res;
+        vector<vector<int>> ans;
+        vector<int> ds;
+        findsum(0,target,candidates,ans,ds);
+        return ans;
     }
 };
