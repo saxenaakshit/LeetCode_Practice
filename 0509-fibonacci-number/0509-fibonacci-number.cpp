75d9e1185b004767c04fccbf5189c06a65b7173e
Akshit Saxena <102389628+saxenaakshit@users.noreply.github.com>
2023-04-17 20:24:39 +0530

    Time: 19 ms (10.67%), Space: 5.9 MB (69.49%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     int fib(int n) {
-        int first=0;
-        int second=1;
-        int ans=0;
+        
         if(n==0){
             return 0;
         }
-       else if(n==1){
+        if(n==1){
             return 1;
-       }
-        for(int i=1;i<n;i++){
-            ans=first+second;
-            first=second;
-            second=ans;
-            
         }
+        int ans=fib(n-1)+fib(n-2);
         return ans;
+        
     }
 };
