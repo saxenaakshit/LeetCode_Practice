aff1844b9706e5c5e9e97b4584d6e51f83f02686
Akshit Saxena <102389628+saxenaakshit@users.noreply.github.com>
2023-06-05 06:39:37 +0530

    Time: 12 ms (88.27%), Space: 13.4 MB (15.81%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void zeroMatrix(vector<vector<int>> &matrix, int n, int m) {
+
+    vector<int> row(n,0); // row array
+  vector<int> col(m,0); // col array
+
+    // Traverse the matrix:
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == 0) {
+                // mark ith index of row wih 1:
+                row[i] = 1;
+
+                // mark jth index of col wih 1:
+                col[j] = 1;
+            }
+        }
+    }
+
+    // Finally, mark all (i, j) as 0
+    // if row[i] or col[j] is marked with 1.
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (row[i] || col[j]) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+}
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        
+        zeroMatrix(matrix,n,m);
+        
+    }
+    
+
+
+
+
+
+};
