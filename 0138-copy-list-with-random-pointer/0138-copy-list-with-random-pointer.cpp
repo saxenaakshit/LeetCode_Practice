aec6bc4eaad92af2a15d48f331df40a23585096b
Akshit Saxena <102389628+saxenaakshit@users.noreply.github.com>
2023-06-16 13:23:18 +0530

    Time: 0 ms (100.00%), Space: 11.2 MB (64.16%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -0,0 +1,65 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution { 
+    private:
+    void insertAtTail(Node* &head,Node* &tail,int d){
+        Node* newNode=new Node(d);
+    if(head==NULL){
+        head=newNode;
+        tail=newNode;
+        return;
+    }
+    else{
+        tail->next=newNode;
+        tail=newNode;
+        
+    }}
+public:
+    Node* copyRandomList(Node* head) {
+        //step-1:-create a clone list
+        Node* cloneHead=NULL;
+        Node* cloneTail=NULL;
+        
+        Node* temp=head;
+        
+        while(temp!=NULL){
+            insertAtTail(cloneHead,cloneTail,temp->val);
+            temp=temp->next;
+        }
+        
+        //step-2 create a map
+        unordered_map<Node*,Node*> oldToNewNode;
+        
+        Node* originalNode=head;
+        Node* cloneNode=cloneHead;
+        while(originalNode!=NULL && cloneNode!=NULL){
+             oldToNewNode[originalNode]=cloneNode;
+            originalNode=originalNode->next;
+            cloneNode=cloneNode->next;
+        }
+        
+        //step-3:Cloning the random pointer
+        originalNode=head;
+        cloneNode=cloneHead;
+        while(originalNode!=NULL){
+            cloneNode->random=oldToNewNode[originalNode->random];
+            originalNode=originalNode->next;
+            cloneNode=cloneNode->next;
+        }
+        return cloneHead;
+    }
+};
